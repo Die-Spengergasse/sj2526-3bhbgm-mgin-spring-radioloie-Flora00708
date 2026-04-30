31366d2f4fd2d29776ebb689935721db0134732e
cernelic <cer230364@spengergasse.at>
2026-04-30 15:43:20 +0200

    Fertige Aufgabe 11 (Vresion 2)

--- src/main/java/at/spengergasse/spring_thymeleaf/controllers/PatientController.java
+++ src/main/java/at/spengergasse/spring_thymeleaf/controllers/PatientController.java
@@ -5,8 +5,11 @@ import at.spengergasse.spring_thymeleaf.entities.*;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
@@ -42,7 +45,20 @@ public class PatientController {
     }
 
     @PostMapping("/add")
-    public String addPatient(@ModelAttribute("patient") Patient patient) {
+    public String addPatient(Model model, @ModelAttribute("patient") Patient patient, BindingResult result) throws Exception {
+
+        //Gebdat
+        if (patient.getBirth().isAfter(LocalDate.now())) {
+            model.addAttribute("error", "Geburtsdatum darf nicht in der Zukunft liegen!");
+            return "add_patient";
+        }
+        //ssn
+        String ssn = String.valueOf(patient.getSsn());
+        if (ssn.length() != 10) {
+            model.addAttribute("error", "SVN muss genau 10 Stellen haben!");
+            return "add_patient";
+        }
+        
         patientRepository.save(patient);
         return  "redirect:/patient/list";
     }
@@ -56,13 +72,41 @@ public class PatientController {
 
     }
     @PostMapping("/addRev")
-    public String addRev(@ModelAttribute("reservation") Reservation reservation, @RequestParam("patientId") int patientId, @RequestParam("machineId") int machineId,  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @RequestParam("reservationDateTime") LocalDateTime datetime) {
-        reservation.setReservationDateTime(datetime);
+    public String addRev(Model model, @ModelAttribute("reservation") Reservation reservation, @RequestParam("patientId") int patientId, @RequestParam("machineId") int machineId,  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @RequestParam("reservationDateTime") LocalDateTime datetime) {
         patientRepository.findById(patientId).ifPresent(reservation::setPatient);
         machineRepository.findById(machineId).ifPresent(reservation::setMachine);
-        reservationRepository.save(reservation);
+        reservation.setReservationDateTime(datetime);
+
+        // Maschinene prüfung
+        if (reservationRepository.existsByMachineAndReservationDateTime(reservation.getMachine(), reservation.getReservationDateTime())) {
+            model.addAttribute("error", "Gerät ist schon belegt um die Uhrzeit");
+
+            model.addAttribute("patients", patientRepository.findAll());
+            model.addAttribute("machines", machineRepository.findAll());
+
+            return "add_rev";
+        }
+        //Patienten
+        if (reservationRepository.existsByPatientAndReservationDateTime(reservation.getPatient(), reservation.getReservationDateTime())) {
+            model.addAttribute("error", "Patient hat schon einen Termin um die Uhrzeit");
+
+            model.addAttribute("patients", patientRepository.findAll());
+            model.addAttribute("machines", machineRepository.findAll());
+
+            return "add_rev";
+        }
+        //termin
+        if (datetime.isBefore(LocalDateTime.now())) {
+            model.addAttribute("error", "Termin darf nicht in der Vergangenheit liegen!");
+            model.addAttribute("patients", patientRepository.findAll());
+            model.addAttribute("machines", machineRepository.findAll());
+            return "add_rev";
+        }
+
+        reservationRepository.save(reservation);  // erst ganz am Ende!
         return "redirect:/patient/list";
     }
+
     @GetMapping("/reservationsByMachine")
     public String reservationsByMachine(Model model) {
         Map<Machine, List<Reservation>> machineReservations = new LinkedHashMap<>();
@@ -75,5 +119,16 @@ public class PatientController {
 
         return "revlist";
     }
-
+    @ExceptionHandler(Exception.class)
+    public ModelAndView errors(Exception exception) {
+        System.out.println(exception);
+        //extra für DB fehler:
+        if (exception.getMessage() != null && exception.getMessage().contains("Communications link failure")) {
+            String message = "Datenbankverbindung fehlgeschlagen – läuft MySQL?";
+        }
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("error", exception.getMessage());
+        return modelAndView;
+    }
+    
 }
--- src/main/java/at/spengergasse/spring_thymeleaf/entities/ReservationRepository.java
+++ src/main/java/at/spengergasse/spring_thymeleaf/entities/ReservationRepository.java
@@ -3,9 +3,15 @@ package at.spengergasse.spring_thymeleaf.entities;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation,Integer> {
     List<Reservation>  findByMachine(Machine machine);
+    //im repository, weil es sonst keinen zugriff auf die db hat
+    // Prüft Reservation Datetime für maschine
+    boolean existsByMachineAndReservationDateTime(Machine machine, LocalDateTime dateTime);
+    // prüft ob patient schon besetzt
+    boolean existsByPatientAndReservationDateTime(Patient patient, LocalDateTime dateTime);
 }
